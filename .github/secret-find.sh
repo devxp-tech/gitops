086b68ce6ea734b672e2a360f6240a3bb4c93c5a
Diego Luisi <dluisi@evolution.com>
2023-03-14 11:54:04 +0000

    :zap: 0.1.72 Add Deployment

--- .github/secret-find.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-if find apps/ -name '*.yaml' | xargs grep -q "kind: Secret"; then
-    echo "Fail, secret found! :("
-    exit 1
-else
-    echo "Success, secret not found! :)"
-    exit 0
-fi
